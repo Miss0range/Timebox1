f4f46e69aba4d91904726cf0425aa27b513ae8ba
Miss0range <jiachen1994@hotmail.com>
2018-01-16 18:18:56 -0500

    Test

---
+++ tb1/App.java
@@ -0,0 +1,15 @@
+package Timebox1.timebox1;
+
+import com.ibm.watson.developer_cloud.discovery.v1.Discovery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Discovery discovery = new Discovery();
+    }
+}
---
+++ tb1/src/main/java/Timebox1/tb1/App.java
@@ -0,0 +1,13 @@
+package Timebox1.tb1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
