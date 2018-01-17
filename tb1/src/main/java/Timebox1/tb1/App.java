fb66cedb42156d1d7904dcc9c17bedaec3699f22
Miss0range <jiachen1994@hotmail.com>
2018-01-16 23:30:45 -0500

    Discovery config

--- tb1/src/main/java/Timebox1/tb1/App.java
+++ tb1/src/main/java/Timebox1/tb1/App.java
@@ -1,5 +1,9 @@
 package Timebox1.tb1;
 
+import com.ibm.watson.developer_cloud.discovery.v1.Discovery;
+import com.ibm.watson.developer_cloud.discovery.v1.model.environment.CreateEnvironmentRequest;
+import com.ibm.watson.developer_cloud.discovery.v1.model.environment.CreateEnvironmentResponse;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Discovery discovery = new Discovery("2017-11-07");
+    	discovery.setEndPoint("https://gateway.watsonplatform.net/discovery/api/");
+    	discovery.setUsernameAndPassword("{username}", "{password}");
+    	String environmentName = "my_environment";
+    	String environmentDesc = "My environment";
+
+    	CreateEnvironmentRequest.Builder createRequestBuilder = new CreateEnvironmentRequest.Builder(environmentName, CreateEnvironmentRequest.Size.ONE);
+    	createRequestBuilder.description(environmentDesc);
+    	CreateEnvironmentResponse createResponse = discovery.createEnvironment(createRequestBuilder.build()).execute();
     }
 }
